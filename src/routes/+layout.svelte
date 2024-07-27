07cb4214048d762e87e3132b3eb7d9ca1bc0b2d0
Anu (they/them) <91222945+anulepau@users.noreply.github.com>
2024-07-27 10:52:58 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { theme } from "$lib/stores"
+  import "../styles/app.scss";
+</script>
+
+<svelte:head>
+  <meta name="color-scheme" content={$theme == 'system' ? 'light dark' : $theme}/>
+  <link rel="stylesheet" href={`src/styles/theme/${$theme}.scss`} />
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+</script>
+
+<div class="welcome-page">
+    <div class="enter-btn-bg">
+      <a class="enter-btn" data-sveltekit-preload-data="tap" href="/home">
+        Enter
+      </a>
+    </div>
+</div>
+
+<style lang="scss">
+  @import '../styles/colors.scss';
+
+  .welcome-page {
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .enter-btn-bg {
+    padding: 0.5rem;
+    background-color: $pp-pink-100;
+    width: fit-content;
+    height: fit-content;
+    border-radius: 1rem;
+  }
+
+  .enter-btn {
+    background-color: $pp-pink-100;
+    border: 2px solid white;
+    border-radius: 1rem;
+    padding: 0 5rem;
+    color: white;
+    font-size: 40px;
+    text-decoration: none;
+    
+    &:hover {
+      transition: 0.2s;
+      background-color: $pp-pink-200;
+      cursor: pointer;
+    }
+  }
+</style>
