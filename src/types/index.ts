9f7238b2136753a2b1d89b5a79df12cab7a37452
Anu (they/them) <91222945+anulepau@users.noreply.github.com>
2024-08-05 18:53:54 -0700

    variable type

--- src/types/index.ts
+++ src/types/index.ts
@@ -26,36 +26,5 @@ export type PokemonTypeListItem = {
 };
 
 export type PokemonTypes = {
-	[key: string]: {
-		name: string;
-		displayName: string;
-		styles: {
-			bg: {
-				primaryColor: string;
-				primaryHoverColor: string;
-				secondaryColor: string;
-				secondaryHoverColor: string;
-			};
-			text: {
-				primaryColor: string;
-				primaryHoverColor: string;
-				secondaryColor: string;
-				secondaryHoverColor: string;
-			};
-			border: {
-				primaryColor: string;
-				primaryHoverColor: string;
-				secondaryColor: string;
-				secondaryHoverColor: string;
-			};
-			ring: {
-				primaryFocusColor: string;
-				secondaryFocusColor: string;
-			};
-			accent: {
-				primaryColor: string;
-				secondaryColor: string;
-			};
-		};
-	};
+	[key: string]: PokemonTypeListItem;
 };
